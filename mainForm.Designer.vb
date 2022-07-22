377903f2dd216809aaa60ee50c733ef89f4f4abd
manny-uncharted <omoebun52@gmail.com>
2022-07-22 22:03:29 +0100

    Add project files.

---
+++ mainForm.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class mainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.startServer = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'startServer
+        '
+        Me.startServer.Location = New System.Drawing.Point(96, 56)
+        Me.startServer.Name = "startServer"
+        Me.startServer.Size = New System.Drawing.Size(261, 81)
+        Me.startServer.TabIndex = 0
+        Me.startServer.Text = "Start Server"
+        Me.startServer.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.startServer)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents startServer As Button
+End Class
---
+++ mainForm.vb
@@ -0,0 +1,54 @@
+﻿Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+
+
+Public Class mainForm
+
+    Dim _server As TcpListener
+    Dim _listOfClients As New List(Of TcpClient)
+
+    Private Sub startServer_Click(sender As Object, e As EventArgs) Handles startServer.Click
+        Try
+            Dim ip As String = "127.0.0.1"
+            Dim port As Integer = 5432
+
+            _server = New TcpListener(IPAddress.Parse(ip), port)
+            _server.Start()
+
+            Threading.ThreadPool.QueueUserWorkItem(AddressOf NewClient)
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub NewClient(state As Object)
+        Dim client As TcpClient = _server.AcceptTcpClient()
+        Try
+            _listOfClients.Add(client)
+            Threading.ThreadPool.QueueUserWorkItem(AddressOf NewClient)
+
+            While True
+                Dim ns As NetworkStream = client.GetStream()
+
+                Dim toReceive(1000000) As Byte
+
+                ns.Read(toReceive, 0, toReceive.Length)
+                Dim txt As String = Encoding.ASCII.GetString(toReceive)
+                For Each c As TcpClient In _listOfClients
+                    If c IsNot client Then
+                        Dim nns As NetworkStream = c.GetStream()
+                        nns.Write(Encoding.ASCII.GetBytes(txt), 0, txt.Length)
+                    End If
+                Next
+            End While
+
+        Catch ex As Exception
+            If _listOfClients.Contains(client) Then
+                _listOfClients.Remove(client)
+            End If
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+End Class
